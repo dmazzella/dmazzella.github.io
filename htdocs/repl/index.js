d8833ca869afe14a35cb33ac9de2a110e00e41fa
dmazzella <damianomazzella@gmail.com>
2017-11-02 23:07:11 +0100

    Update GUI

--- htdocs/repl/index.js
+++ htdocs/repl/index.js
@@ -32,6 +32,8 @@ toastr.options = {
     "hideMethod": "fadeOut"
 };
 
+$('[data-toggle="tooltip"]').tooltip();
+
 /* @@@@@@@@@@@@@@@@@@@@@@@@ Bluetooth Web API @@@@@@@@@@@@@@@@@@@@@@@@ */
 const nus_primary_service_uuid = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const nus_rx_characteristic_uuid = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
